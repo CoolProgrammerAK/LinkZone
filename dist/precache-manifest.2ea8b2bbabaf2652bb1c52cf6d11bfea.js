1ecda7ec7cb43e265500ef664801178fc221f08c
CoolProgrammerAK <68710643+CoolProgrammerAK@users.noreply.github.com>
2020-07-24 18:53:16 +0530

    Add files via upload

---
+++ dist/precache-manifest.2ea8b2bbabaf2652bb1c52cf6d11bfea.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ac57c39f446d421883467c4beaf5dab",
+    "url": "/index.html"
+  },
+  {
+    "revision": "55b497e903996a56850b",
+    "url": "/static/css/main.0c9bb225.chunk.css"
+  },
+  {
+    "revision": "bbdf7dda4ad69a66582b",
+    "url": "/static/js/2.5783b17e.chunk.js"
+  },
+  {
+    "revision": "5794897cf661d4284a7f0a757ca87b51",
+    "url": "/static/js/2.5783b17e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "55b497e903996a56850b",
+    "url": "/static/js/main.ca27fd62.chunk.js"
+  },
+  {
+    "revision": "0017902968bd0847d330",
+    "url": "/static/js/runtime-main.5fc18df1.js"
+  },
+  {
+    "revision": "a274ad5f1a1c912b3e071dd268494b57",
+    "url": "/static/media/home_background.a274ad5f.jpg"
+  },
+  {
+    "revision": "8a4d2a2166abd2fef635ac9681a82023",
+    "url": "/static/media/logo1.8a4d2a21.png"
+  }
+]);
