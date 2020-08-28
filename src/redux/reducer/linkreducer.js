2e48c37937e119527aa0bac2f9aa1caf2d4fd470
CoolProgrammerAK <68710643+CoolProgrammerAK@users.noreply.github.com>
2020-08-28 12:27:01 +0530

    Add files via upload

--- src/redux/reducer/linkreducer.js
+++ src/redux/reducer/linkreducer.js
@@ -1,11 +1,11 @@
-import { LOADING_DATA, SET_LINK, LINK_ERROR ,CLEAR_ERROR, OPEN_ALERT, HIDE_ALERT, DELETEHIDE_ALERT,DELETE_ALERT,UPDATE_DATA,UPDATE_ERROR} from "../type/type"
+import { LOADING_DATA, SET_LINK, LINK_ERROR ,CLEAR_ERROR, OPEN_ALERT, HIDE_ALERT, DELETEHIDE_ALERT,DELETE_ALERT,UPDATE_DATA,UPDATE_ERROR, UPDATE_ALERT, UPDATEHIDE_ALERT} from "../type/type"
 
 const initialState = {
 loadingdata:false,
 links:[],
 errors:{},
 open:false,
-open_del:false
+open_del:false,open_up:false
 }
 
 export default (state = initialState, action) => {
@@ -13,48 +13,56 @@ export default (state = initialState, action) => {
 
     case LOADING_DATA:
         return {...state,loadingdata:true,errors:{},
-    open:false,open_del:false}
+    open:false,open_del:false,open_up:false}
     case SET_LINK:
         return {
             loadingdata:false,
             links:[...state.links,{...action.payload,uid:action.uid}],
             errors:{},
-            open:false,open_del:false
+            open:false,open_del:false,open_up:false
         }
     case LINK_ERROR:
         return {
             ...state,
             loadingdata:false,
             errors: {linkerror:action.err.message},
-            open:false,open_del:false
+            open:false,open_del:false,open_up:false
         }
     case CLEAR_ERROR:
         return {
-          ...state,errors:{},loadingdata:false,open:false,open_del:false
+          ...state,errors:{},loadingdata:false,open:false,open_del:false,open_up:false
         }
     case OPEN_ALERT:
         return{
-            ...state,open:true,open_del:false
+            ...state,open:true,open_del:false,open_up:false
         }
     case HIDE_ALERT:
         return{
-            ...state,open:false,open_del:false
+            ...state,open:false,open_del:false,open_up:false
         }
         case DELETE_ALERT:
             return{
-                ...state,open:false,open_del:true
+                ...state,open:false,open_del:true,open_up:false
             }
         case DELETEHIDE_ALERT:
             return{
-                ...state,open:false,open_del:false
+                ...state,open:false,open_del:false,open_up:false
             }
+            case UPDATE_ALERT:
+                return{
+                    ...state,open:false,open_del:false,open_up:true
+                }
+            case UPDATEHIDE_ALERT:
+                return{
+                    ...state,open:false,open_del:false,open_up:false
+                }
 
         case UPDATE_DATA:
             return {
                 loadingdata:false,
             links:[...state.links,{...action.payload,uid:action.uid}],
             errors:{},
-            open:false,open_del:false
+            open:false,open_del:false,open_up:false
             }
             case UPDATE_ERROR:
                 return {
--- src/redux/reducer/userreducer.js
+++ src/redux/reducer/userreducer.js
@@ -1,38 +1,38 @@
-import {LOADING_UI, SIGNUP_ERROR, SET_USER,LOGIN_SUCCESS,LOGIN_ERROR,SIGNUP_SUCCESS, IMAGE_ERROR, THEME_LINK, THEME_LINKOFF} from '../type/type'
+import {LOADING_UI, SIGNUP_ERROR, SET_USER,LOGIN_SUCCESS,LOGIN_ERROR,SIGNUP_SUCCESS, IMAGE_ERROR, THEME_LINK, THEME_LINKOFF, RESET_LINK, RESET_LINKOFF} from '../type/type'
 const initialState = {
-  credentials:{},errors:{},loading:false,open:false
+  credentials:{},errors:{},loading:false,open:false,link:{open:false,data:"",color:""}
 }
 
 export default (state = initialState, action) => {
     switch (action.type) {
 
     case LOADING_UI:
-        return { ...state,loading:true,errors:{},open:false}
+        return { ...state,loading:true,errors:{},open:false,link:{open:false,data:"",color:""}}
     case SET_USER:
       
         return {
             credentials:action.payload,
             loading:false,
-            errors:{},open:false
+            errors:{},open:false,link:{open:false,data:"",color:""}
         }
     case SIGNUP_ERROR:
     
       return {
         ...state,
         loading:false,
-        errors: {signup:action.err.message},open:false
+        errors: {signup:action.err.message},open:false,link:{open:false,data:"",color:""}
       }
     case SIGNUP_SUCCESS:
  
       return {
         ...state,
-        loading:false,open:false,
+        loading:false,open:false,link:{open:false,data:"",color:""},
         errors: {}}
     case LOGIN_ERROR:
   
       return {
         ...state,loading:false,
-        errors: {login:action.err.message},open:false
+        errors: {login:action.err.message},open:false,link:{open:false,data:"",color:""}
       }
 
     case LOGIN_SUCCESS:
@@ -40,7 +40,7 @@ export default (state = initialState, action) => {
       return {
         ...state,
         loading:false,
-        errors:{},open:false
+        errors:{},open:false,link:{open:false,data:"",color:""}
       }
 
    
@@ -49,13 +49,13 @@ export default (state = initialState, action) => {
         return {
           ...state,
           loading:false,
-          errors: {image:action.err.message},open:false
+          errors: {image:action.err.message},open:false,link:{open:false,data:"",color:""}
         }
       case SIGNUP_SUCCESS:
    
         return {
           ...state,
-          loading:false,open:false,
+          loading:false,open:false,link:{open:false,data:"",color:""},
           errors: {}}
         case THEME_LINK:
           return{
@@ -64,6 +64,14 @@ export default (state = initialState, action) => {
           case THEME_LINKOFF:
             return{
               ...state,open:false
+            }
+            case RESET_LINK:
+          return{
+            ...state,link:{open:true,data:action.payload,color:action.color}
+          }
+          case RESET_LINKOFF:
+            return{
+              ...state,link:{open:false,data:""}
             }
     
     default:
