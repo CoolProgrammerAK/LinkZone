59ee69ed41bce6738961fad6b90d627d4a4f3fca
CoolProgrammerAK <68710643+CoolProgrammerAK@users.noreply.github.com>
2020-08-28 12:35:35 +0530

    Update firebase.js

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -4,13 +4,13 @@ import 'firebase/auth';
 import 'firebase/storage'
 
 var firebaseConfig = {
-    apiKey: "AIzaSyDD1trMzOZBS_6gTkpX7C30fapLE__hOLE",
-    authDomain: "webapp-851e5.firebaseapp.com",
-    databaseURL: "https://webapp-851e5.firebaseio.com",
-    projectId: "webapp-851e5",
-    storageBucket: "webapp-851e5.appspot.com",
-    messagingSenderId: "235557315004",
-    appId: "1:235557315004:web:f502883448065559544bb9"
+    apiKey: "XXXX",
+    authDomain: "XXXX",
+    databaseURL: "XXXX",
+    projectId: "XXXX",
+    storageBucket: "XXXX",
+    messagingSenderId: "XXXX",
+    appId: "XXXXX"
   };
   
   firebase.initializeApp(firebaseConfig)
